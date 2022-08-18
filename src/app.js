f30a90878036cb608345d11480a93781a8c35f77
Krzysztof Mendroch <mendrochk@gmail.com>
2022-08-18 11:37:08 +0200

    Dodanie widoku ekranu głównego

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { createWelcome } from './generatingviews.js'
+import { createWelcome, createMainPanel } from './generatingviews.js'
 
 class Prayer {
     constructor() {
@@ -6,20 +6,6 @@ class Prayer {
         createWelcome()
     }
 
-
-
-    // Tworzenie testowej tablicy w Local Storage
-
-    // createFileLocalStorage = () => {
-    //     let quotes = []
-    //     quotes[0] = ['Wczoraj do Ciebie nie należy. Jutro niepewne. Tylko dziś jest Twoje', 'św. Jan Paweł II']
-    //     quotes[1] = ['Bądźmy zawsze radośni, a czas minie szybko', 'św. Jan Bosko']
-    //     quotes[2] = ['Moim zadaniem jest głosić, nie przekonywać', 'św. Benedyktyna']
-    //     quotes[3] = ['Moim zdaniem to nie ma tak, że dobrze albo, że nie dobrze', 'jakiś typ z Asterix i Obelix']
-    //     quotes[4] = ['Dość tego szukania cytatów', 'Anonim']
-    //     // localStorage.setItem('quotes', JSON.stringify(quotes));
-    //     console.log(quotes)
-    // }
 }
 
 document.addEventListener('DOMContentLoaded', new Prayer())
--- src/generatingviews.js
+++ src/generatingviews.js
@@ -16,6 +16,7 @@ const destroyWelcome = () => {
     body.removeEventListener('click', destroyWelcome)
     body.classList.remove('gradient')
     body.innerHTML = ''
+    createMainPanel()
 }
 
 export const createWelcome = () => {
@@ -26,9 +27,9 @@ export const createWelcome = () => {
     const welcome = createDOMElem('div', 'welcome')
     const welcomeMain = createDOMElem('div', 'welcome-main')
     const welcomeMainIcon = createDOMElem('img', 'welcome-main__icon', null, '../img/pray.png')
-    const welcomeMainText = createDOMElem('p', 'welcome-main__text welcome_text', quote[0])
+    const welcomeMainText = createDOMElem('p', 'welcome_text', quote[0])
     const welcomeMainAuthor = createDOMElem('p', 'welcome-main__author welcome_text', quote[1])
-    const welcomeFooterText = createDOMElem('p', 'welcome-footer_text welcome_text', 'Dotknij ekranu aby kontynuować...')
+    const welcomeFooterText = createDOMElem('p', 'welcome-footer__text welcome_text', 'Dotknij ekranu aby kontynuować...')
 
     welcome.appendChild(welcomeMain)
     welcome.appendChild(welcomeFooterText)
@@ -37,4 +38,38 @@ export const createWelcome = () => {
     welcomeMain.appendChild(welcomeMainAuthor)
 
     body.appendChild(welcome)
+}
+
+// Tworzenie ekranu głównego
+export const createMainPanel = () => {
+    const main = createDOMElem('div')
+
+    const mainHeader = createDOMElem('header', 'home-header')
+    const mainHeaderContainer = createDOMElem('div', 'container home-header__container')
+    const mainHeaderImg = createDOMElem('img', 'home-header__menu-icon', null, '../img/menu.png')
+    const mainHeaderHeader = createDOMElem('h1', 'home-header__header', 'Ekran główny')
+
+    const mainContainer = createDOMElem('main', 'container')
+    const mainHomeElem = createDOMElem('div', 'home-elem')
+    const mainHomeElemImgDiv = createDOMElem('div', 'home-elem__img-div')
+    const mainHomeElemImg = createDOMElem('img', 'home-elem__img', null, '../img/music-book.png')
+    const mainHomeElemTexts = createDOMElem('div', 'home-elem__texts')
+    const mainHomeElemHeader = createDOMElem('p', 'home-elem__texts-header', 'PIEŚNI')
+    const mainHomeElemStrap = createDOMElem('div', 'home-elem__texts-strap')
+    const mainHomeElemDirecting = createDOMElem('p', 'home-elem__texts-directing', 'PRZEJDŹ DO PIEŚNI')
+
+    main.appendChild(mainHeader)
+    main.appendChild(mainContainer)
+    mainHeader.appendChild(mainHeaderContainer)
+    mainHeaderContainer.appendChild(mainHeaderImg)
+    mainHeaderContainer.appendChild(mainHeaderHeader)
+    mainContainer.appendChild(mainHomeElem)
+    mainHomeElem.appendChild(mainHomeElemImgDiv)
+    mainHomeElemImgDiv.appendChild(mainHomeElemImg)
+    mainHomeElem.appendChild(mainHomeElemTexts)
+    mainHomeElemTexts.appendChild(mainHomeElemHeader)
+    mainHomeElemTexts.appendChild(mainHomeElemStrap)
+    mainHomeElemTexts.appendChild(mainHomeElemDirecting)
+
+    body.appendChild(main)
 }
