ed53de4b12924b5b0b5df8808f571df021a143b1
Krzysztof Mendroch <mendrochk@gmail.com>
2022-09-07 08:08:59 +0200

    Poprawienie wyglądu popup wszystkich ustawień czcionki w widoku pieśni

--- src/app.js
+++ src/app.js
@@ -24,7 +24,8 @@ class Prayo {
         this.setQuote()
         this.setSettings()
         this.setSettingsTexts()
-        this.fetchAndDisplaySongs()  // <- funkcja testowa
+        this.createCustomSelect()
+        // this.fetchAndDisplaySongs()  // <- funkcja testowa
     }
 
     fetchAndDisplaySongs = () => {
@@ -86,9 +87,9 @@ class Prayo {
         this.viewElems.fontLineHeightBtn.addEventListener('click', () => {
             this.changeFontSettings(this.listOfRadioFontLineHeight, 'fontLineHeight')
         })
-        this.viewElems.fontTypeSelect.addEventListener('change', this.changeAllFontSettings)
-        this.viewElems.fontSizeSelect.addEventListener('change', this.changeAllFontSettings)
-        this.viewElems.fontLineHeightSelect.addEventListener('change', this.changeAllFontSettings)
+        // this.viewElems.fontTypeSelect.addEventListener('change', this.changeAllFontSettings)
+        // this.viewElems.fontSizeSelect.addEventListener('change', this.changeAllFontSettings)
+        // this.viewElems.fontLineHeightSelect.addEventListener('change', this.changeAllFontSettings)
     }
 
     setQuote = () => {
@@ -439,6 +440,101 @@ class Prayo {
         setTextSettings(this.viewElems.fontTypeSelect.value, this.viewElems.fontSizeSelect.value, this.viewElems.fontLineHeightSelect.value)
         this.setSettings()
         this.setSettingsTexts()
+    }
+
+    createCustomSelect = () => {
+        let fontClasses = [
+            "is-font-type--Inter",
+            "is-font-type--ArchivoNarrow",
+            "is-font-type--Anaheim",
+            "is-font-type--EBGaramond",
+            "is-font-type--PTSans",
+            "is-font-type--Raleway"
+        ]
+        var x, i, j, l, ll, selElmnt, a, b, c;
+        /*look for any elements with the class "custom-select":*/
+        x = document.getElementsByClassName("custom-select");
+        l = x.length;
+        for (i = 0; i < l; i++) {
+            let changeAllFontSettings = this.changeAllFontSettings
+            selElmnt = x[i].getElementsByTagName("select")[0];
+            ll = selElmnt.length;
+            /*for each element, create a new DIV that will act as the selected item:*/
+            a = document.createElement("DIV");
+            a.classList.add('select-selected')
+            a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+            x[i].appendChild(a);
+            /*for each element, create a new DIV that will contain the option list:*/
+            b = document.createElement("DIV");
+            b.classList.add('select-items')
+            b.classList.add('select-hide')
+            for (j = 1; j < ll; j++) {
+                /*for each option in the original select element,
+                create a new DIV that will act as an option item:*/
+                c = document.createElement("DIV");
+                c.innerHTML = selElmnt.options[j].innerHTML;
+                if (i === 0) {
+                    c.classList.add(fontClasses[j-1])   
+                }+
+                c.addEventListener("click", function(e) {
+                    /*when an item is clicked, update the original select box,
+                    and the selected item:*/
+                    var y, i, k, s, h, sl, yl;
+                    s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                    sl = s.length;
+                    h = this.parentNode.previousSibling;
+                    for (i = 0; i < sl; i++) {
+                        if (s.options[i].innerHTML == this.innerHTML) {
+                            s.selectedIndex = i;
+                            h.innerHTML = this.innerHTML;
+                            y = this.parentNode.getElementsByClassName("same-as-selected");
+                            yl = y.length;
+                            for (k = 0; k < yl; k++) {
+                                y[k].classList.remove("same-as-selected");
+                            }
+                            this.classList.add("same-as-selected")
+                            break;
+                        }
+                    }
+                    h.click();
+                    changeAllFontSettings()
+                });
+                b.appendChild(c);
+            }
+            x[i].appendChild(b);
+            a.addEventListener("click", function(e) {
+                /*when the select box is clicked, close any other select boxes,
+                and open/close the current select box:*/
+                e.stopPropagation();
+                closeAllSelect(this);
+                this.nextSibling.classList.toggle("select-hide");
+                this.classList.toggle("select-arrow-active");
+            });
+        }
+        function closeAllSelect(elmnt) {
+        /*a function that will close all select boxes in the document,
+        except the current select box:*/
+        var x, y, i, xl, yl, arrNo = [];
+        x = document.getElementsByClassName("select-items");
+        y = document.getElementsByClassName("select-selected");
+        xl = x.length;
+        yl = y.length;
+        for (i = 0; i < yl; i++) {
+            if (elmnt == y[i]) {
+            arrNo.push(i)
+            } else {
+            y[i].classList.remove("select-arrow-active");
+            }
+        }
+        for (i = 0; i < xl; i++) {
+            if (arrNo.indexOf(i)) {
+            x[i].classList.add("select-hide");
+            }
+        }
+        }
+        /*if the user clicks anywhere outside the select box,
+        then close all select boxes:*/
+        document.addEventListener("click", closeAllSelect);
     }
 }
 
